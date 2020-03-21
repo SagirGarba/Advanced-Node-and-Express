b079ed00214bd97ae1767f87ae86497b45993528
Glitch (mousy-marred-kale) <none>
2020-03-21 04:05:08 +0000

    🌝👹 Updated with Glitch

--- server.js
+++ server.js
@@ -4,60 +4,33 @@ const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const session     = require('express-session');
-const passport    = require('passport');
 const mongo       = require('mongodb').MongoClient;
-const ObjectID    = require('mongodb').ObjectID;
-const LocalStrategy = require('passport-local');
-const bcrypt = require('bcrypt');
+const passport    = require('passport');
+const GitHubStrategy = require('passport-github').Strategy;
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug')
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
 mongo.connect(process.env.DATABASE, (err, db) => {
     if(err) {
         console.log('Database error: ' + err);
     } else {
         console.log('Successful database connection');
-
-        passport.serializeUser((user, done) => {
-          done(null, user._id);
-        });
-
-        passport.deserializeUser((id, done) => {
-            db.collection('users').findOne(
-                {_id: new ObjectID(id)},
-                (err, doc) => {
-                    done(null, doc);
-                }
-            );
-        });
       
-        passport.use(new LocalStrategy(
-          function(username, password, done) {
-            db.collection('users').findOne({ username: username }, function (err, user) {
-              console.log('User '+ username +' attempted to log in.');
-              if (err) { return done(err); }
-              if (!user) { return done(null, false); }
-              if (!bcrypt.compareSync(password, user.password)) { return done(null, false); }
-              return done(null, user);
-            });
-          }
-        ));
-      
+        app.use(session({
+          secret: process.env.SESSION_SECRET,
+          resave: true,
+          saveUninitialized: true,
+        }));
+        app.use(passport.initialize());
+        app.use(passport.session());
       
         function ensureAuthenticated(req, res, next) {
           if (req.isAuthenticated()) {
@@ -65,69 +38,91 @@ mongo.connect(process.env.DATABASE, (err, db) => {
           }
           res.redirect('/');
         };
-      
 
+        passport.serializeUser((user, done) => {
+          done(null, user.id);
+        });
+
+        passport.deserializeUser((id, done) => {
+            db.collection('socialusers').findOne(
+                {id: id},
+                (err, doc) => {
+                    done(null, doc);
+                }
+            );
+        });
+
+      
+        /*
+        *  ADD YOUR CODE BELOW
+        */
+      
+      
+        passport.use(new GitHubStrategy({
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: "https://guttural-birch.gomix.me/auth/github/callback"
+          },
+          function(accessToken, refreshToken, profile, cb) {
+              console.log(profile);
+              db.collection('socialusers').findAndModify(
+                  {id: profile.id},
+                  {},
+                  {$setOnInsert:{
+                      id: profile.id,
+                      name: profile.displayName || 'John Doe',
+                      photo: profile.photos[0].value || '',
+                      email: profile.emails[0].value || 'No public email',
+                      created_on: new Date(),
+                      provider: profile.provider || ''
+                  },$set:{
+                      last_login: new Date()
+                  },$inc:{
+                      login_count: 1
+                  }},
+                  {upsert:true, new: true}, //Insert object if not found, Return new object after modify
+                  (err, doc) => {
+                      return cb(null, doc.value);
+                  }
+              );
+            }
+        ));
+      
+        app.route('/auth/github')
+          .get(passport.authenticate('github'));
+      
+        app.route('/auth/github/callback')
+          .get(passport.authenticate('github', { failureRedirect: '/' }), (req,res) => {
+              res.redirect('/profile');
+          });
+      
+        /*
+        *  ADD YOUR CODE ABOVE
+        */
+      
+      
         app.route('/')
           .get((req, res) => {
-            res.render(process.cwd() + '/views/pug/index', {title: 'Home Page', message: 'login', showLogin: true, showRegistration: true});
+            res.render(process.cwd() + '/views/pug/index');
           });
-           
-      
-        app.route('/login')
-          .post(passport.authenticate('local', { failureRedirect: '/' }),(req,res) => {
-               res.redirect('/profile');
-          });
-      
+
         app.route('/profile')
           .get(ensureAuthenticated, (req, res) => {
-               res.render(process.cwd() + '/views/pug/profile', {username: req.user.username});
+               res.render(process.cwd() + '/views/pug/profile', {user: req.user});
           });
-      
-             
-      
-       app.route('/register')
-          .post((req, res, next) => {
-         
-              db.collection('users').findOne({ username: req.body.username }, function (err, user) {
-                  if(err) {
-                      next(err);
-                  } else if (user) {
-                      res.redirect('/');
-                    var hash = bcrypt.hashSync(req.body.password, 12)
-                  } else {
-                      db.collection('users').insertOne(
-                        {username: req.body.username,
-                         password: hash},
-                        (err, doc) => {
-                            if(err) {
-                                res.redirect('/');
-                            } else {
-                                res.redirect(null, user);
-                            }
-                        }
-                      )
-                  }
-              })},
-                
-                
-            passport.authenticate('local', { failureRedirect: '/' }),
-            (req, res, next) => {
-                res.redirect('/profile');
-            }
-        );
-      
-      app.route('/logout')
+
+        app.route('/logout')
           .get((req, res) => {
               req.logout();
               res.redirect('/');
           });
-      
-      app.use((req, res, next) => {
+
+        app.use((req, res, next) => {
           res.status(404)
             .type('text')
             .send('Not Found');
         });
-
+      
         app.listen(process.env.PORT || 3000, () => {
           console.log("Listening on port " + process.env.PORT);
         });  
